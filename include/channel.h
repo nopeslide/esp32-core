2a433307d8634c83a053048bc377f2995eef6d26
drechsler <drechsler@tu-berlin.de>
2020-03-18 18:02:11 +0100

    Added documentation header

--- include/channel.h
+++ include/channel.h
@@ -1,3 +1,41 @@
+// Channels are objects which enable iteration over other Channel objects with
+// the same name/identifier.
+// They are implemented as an item of two separate lists.
+// The "unique" list holds items with different identifiers,
+// the "same" list holds items that share the same identifier. 
+// Each list item itself is a valid list containing only itself;
+// multiple items can be combined to larger list.
+// 
+// This implementation constructs a single valid "unique" list,
+// starting with a globally defined root node. All "same" lists of all items
+// are considered as valid.
+//
+// Adding to this structure implies iterating the "unique" list, until either
+// the same identifier is found and the new item is added to the "same" list
+// of the found item or the end of the "unique" list reached and it is appended
+// as new "unique" list item.
+//
+// Each added Channel object now part of a "same" list which can be directly
+// iterated to find other items.
+//
+// exemplary structure:
+// 
+//              root node---.  O        O
+//                          v  |        |
+// unique identifier list-> O--O--O--O--O  
+//                             |     |
+//                             O     O
+//                             ^
+//      same identifier list---'                    
+//
+// Channel objects itself contain a context pointer, a callback function and
+// an additional flags field. A broadcast function is provided to iterate
+// through all "same" list items of a Channel object. The callback is issued
+// with a data pointer and a timeout value by the caller and the callee
+// specific context and flags. This interface is intended to enable dynamic
+// management of FreeRTOS queues, where a queue can be registered as a context
+// of a Channel and therefore fed by the channels callback.
+
 #ifndef __CHANNEL__
 #define __CHANNEL__
 
