6dfcc9c5f093f7d29a048406f3c6325582ace38f
drechsler <drechsler@tu-berlin.de>
2020-03-18 17:02:44 +0100

    added & moved documentation to header

--- include/channel.h
+++ include/channel.h
@@ -1,8 +1,6 @@
 #ifndef __CHANNEL__
 #define __CHANNEL__
 
-#include <string.h>
-
 // #include <freertos/FreeRTOS.h>
 // #include <freertos/queue.h>
 typedef int BaseType_t;
@@ -29,7 +27,7 @@ typedef struct channel {
 #endif
 
 /**
- * CHANNEL_INIT - Macro to provide initialization code for channel object
+ * CHANNEL_INIT - Macro to provide initialization for channel object
  */
 #define CHANNEL_INIT(name, identifier, ctx, flags, callback) { "" identifier "", LIST_HEAD_INIT(name.same), LIST_HEAD_INIT(name.unique), ctx, flags, callback }
 
@@ -53,6 +51,14 @@ typedef struct channel {
  */
 #define INPUT_QUEUE(name, queue) CHANNEL(name, STR(name), queue, queueSEND_TO_BACK, &xQueueGenericSend)
 
+/**
+ * channel_init - helper function to initialize channel object
+ * @ch: pointer to channel object to initialize
+ * @identifier: identifier string of the channel
+ * @ctx: pointer to context of the callback
+ * @flags: additional flags for the callback to modify behaviour
+ * @callback: function to call on new data  
+ */ 
 static
 void
 inline __attribute__((always_inline))
@@ -66,6 +72,11 @@ channel_init
     ch->callback = callback;
 }
 
+/**
+ * channel_setContext - helper function to change the context the callback acts on
+ * @ch: pointer to channel object to modify
+ * @ctx: pointer to new context
+ */
 static
 void
 inline __attribute__((always_inline))
@@ -74,6 +85,11 @@ channel_setContext
     ch->ctx = ctx;
 }
 
+/**
+ * channel_setCallback - helper function to change the callback
+ * @ch: pointer to channel object to modify
+ * @callback: function to register as callback
+ */
 static
 void 
 inline __attribute__((always_inline))
@@ -82,18 +98,71 @@ channel_setCallback
     ch->callback = callback;
 }
 
+/**
+ * channel_init_broadcast - helper function to initialize position element in broadcast
+ * @ch: pointer to channel object
+ * @ctx: pointer to position element
+ */
 static
 void
 inline __attribute__((always_inline))
 channel_init_broadcast
-(const Channel * const ch, const Channel ** const tmp)
+(const Channel * const ch, const Channel ** const pos)
 {
-    *tmp = ch;
+    *pos = ch;
 }
 
-void channel_register(Channel * const ch);
-void channel_deregister(Channel * const ch);
+/**
+ * channel_register - register a channel
+ * @ch: channel to register
+ * 
+ * This function adds channel to the message passing structure. 
+ * It doesn't make a difference whether this is a message
+ * provider or consumer. The difference is only made by the channels
+ * callback implementation.
+ */
+void
+channel_register
+(Channel * const ch);
 
-const BaseType_t channel_broadcast(Channel * const ch, Channel ** tmp, const void * const data, const TickType_t timeout);
+/**
+ * unregister_channel - removes a channel input or output from the message structure
+ * @ch: channel element to remove
+ */
+void
+channel_deregister
+(Channel * const ch);
+
+/**
+ * channel_broadcast - send a message to all who registered on this channels identifier
+ * @ch: the channel structure that holds informations for this channel
+ * @tmp: iterator pointer, needs to be initialized by channel_init_broadcast or reused to continue broadcast
+ * @data: the pointer to the message
+ * @timeout: how many time-slices each callback may block before aborting
+ * @return: returns pdPass (=1) if broadcast to was successful, if non-one value is returned, iteration was stopped, because a callback failed 
+ * 
+ * Iterates over all elements in the same list, that registered on this channels identifier
+ * and calls the callback function on it. May return with error_code before all elements were notified, allowing for custom error handling
+ * Example for skipping failing callbacks:
+ *  Channel **pos;
+ *  channel_init_broadcast(ch,pos);
+ *  while(!channel_broadcast(ch,pos,data,timeout));
+ */
+const BaseType_t
+channel_broadcast
+(Channel * const ch, Channel ** tmp, const void * const data, const TickType_t timeout);
+
+/**
+ * channel_send - sends a message to a single channel object
+ * @ch: the channel structure that holds informations for this channel
+ * @data: the pointer to the message
+ * @timeout: how many time-slices the callback may block before aborting
+ * @return: returns pdPass (=1) if send was successful, if non-one value is returned, callback failed
+ * 
+ * Calls the callback function on the channel.
+ */
+const BaseType_t
+channel_send
+(Channel * const ch, const void * const data, const TickType_t timeout, const BaseType_t flags);
 
 #endif
--- src/channel.c
+++ src/channel.c
@@ -1,16 +1,10 @@
 #include "channel.h"
 
+#include <string.h>
+
+// root channel of the unique channel list
 static CHANNEL(root, "", NULL, 0, NULL);
 
-/**
- * channel_register - register a channel
- * @channel: channel to register
- * 
- * This function adds channel to the message passing structure. 
- * It doesn't make a difference whether this is a message
- * provider or consumer. The difference is only made by the channels
- * callback implementation.
- */
 void 
 channel_register
 (Channel *ch) 
@@ -26,24 +20,16 @@ channel_register
     list_add(&ch->unique, &root.unique);
 }
 
-/**
- * channel_send - send a message to all who registered on this channels identifier
- * @ch: the channel structure that holds informations for this channel
- * @data: the messages content
- * 
- * Iterates over all elements in the same list, that registered on this channels identifier
- * and calls the push callback function on it. 
- */
 const BaseType_t
 channel_broadcast
-(Channel * const ch, Channel ** tmp, const void * const data, const TickType_t timeout)
+(Channel * const ch, Channel ** pos, const void * const data, const TickType_t timeout)
 {
-    Channel *curr = *tmp;
+    Channel *curr = *pos;
     list_for_each_entry_continue(curr, &ch->same, same) {
         if (curr->callback) {
             BaseType_t status = curr->callback(curr->ctx, data, timeout, curr->flags);
             if (!status) {
-                *tmp = curr;
+                *pos = curr;
                 return status;
             };
         }
@@ -58,13 +44,9 @@ channel_send
     if (!ch->callback) {
         return pdPass;
     }
-    return ch->callback(ch->ctx, data, timeout, flags);
+    return ch->callback(ch->ctx, data, timeout, ch->flags);
 }
 
-/**
- * unregister_channel - removes a channel input or output from the message structure
- * @ch: channel element to remove
- */
 void 
 channel_unregister
 (Channel *ch) 
